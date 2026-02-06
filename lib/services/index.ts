217bbacb0767015d171759c2ebe97b7997f5ebe3
IRASUBIZA-SALY-NELSON <irasubizasalynelson@gmail.com>
2026-02-06 14:43:15 +0200

    added login and adding members and faced  backend error then stopped

--- lib/services/index.ts
+++ lib/services/index.ts
@@ -1,5 +1,5 @@
 // Export all services
 export * from './auth.service';
 export * from './blog.service';
-export * from './member.service';
-export * from './choir.service';
+export { memberService, type Member, type CreateMemberInput, type UpdateMemberInput } from './member.service';
+export { choirService, type Choir, type CreateChoirInput, type UpdateChoirInput, type ChoirSong, type CreateChoirSongInput, type UpdateChoirSongInput } from './choir.service';
